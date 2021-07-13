6831e8b1600fc6c6e046dfa5371ea3d51c77e83e
marlon <marlonmunaya@hotmail.com>
2021-07-13 17:11:52 -0500

    modificado

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-#!flask/bin/python
-from re import U
 from flask import Flask, jsonify, abort, request
+import requests
 
 app = Flask(__name__)
 
@@ -46,8 +45,20 @@ def create_task():
     # return jsonify({'task': task}), 201
     print(destinatario)
     print(mensaje)
-
+    test()
     return '''<h1>The language value is: {} y {}</h1>'''.format(destinatario,mensaje)
+
+def test():    
+    url = 'https://live-server-763.wati.io/api/v1/sendSessionMessage/51937535378'
+    auth_token='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJiODE0MjUzYy1mMDAyLTQ3MTYtYjkwOS0xMmJhY2E3MDc3ZDEiLCJ1bmlxdWVfbmFtZSI6ImFnZW50ZTVAdGVsZW5ldC5wZSIsIm5hbWVpZCI6ImFnZW50ZTVAdGVsZW5ldC5wZSIsImVtYWlsIjoiYWdlbnRlNUB0ZWxlbmV0LnBlIiwiYXV0aF90aW1lIjoiMDcvMTMvMjAyMSAxMzoxOTo0NCIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6IkFETUlOSVNUUkFUT1IiLCJleHAiOjI1MzQwMjMwMDgwMCwiaXNzIjoiQ2xhcmVfQUkiLCJhdWQiOiJDbGFyZV9BSSJ9.0YITXIKLxJHe5Prjt7O53ofcRvi0PNJb-U7TI06cRRE'
+    param = {'messageText':"texto de flask",}
+    headers = {'Content-type': 'application/json ; charset=UTF-8','Authorization': 'Bearer ' + auth_token}
+    response = requests.post(url, params=param, headers=headers)
+    # wait for the response. it should not be higher 
+    # than keep alive time for TCP connection
+    print(response)
+    print(response.json())
+
 
 if __name__ == '__main__':
     app.run(debug=False)
