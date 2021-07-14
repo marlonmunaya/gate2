3ce3d396e57ab276e8a2d007b2481203720e2a0b
marlon <marlonmunaya@hotmail.com>
2021-07-14 10:30:20 -0500

    modificacion

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from typing_extensions import Required
 from flask import Flask, jsonify, abort, request
 import requests
 from flask_jwt_extended import JWTManager
@@ -33,9 +34,10 @@ def get_task(task_id):
 
 @app.route('/todo/api/v1.0/rec', methods=['POST'])
 def create_task():
+    global mensaje
     # if not request.json or not 'title' in request.json:
-    destinatario = request.args.get('destinatario')
-    mensaje = request.args.get('mensaje')
+    # destinatario = request.args.get('destinatario',type=str)
+    mensaje = request.values.get('mensaje',type=str)
     token= request.authorization
     # task = {
     #     'id': tasks[-1]['id'] + 1,
@@ -46,15 +48,15 @@ def create_task():
     # }
     # tasks.append(task)
     # return jsonify({'task': task}), 201
-    print(destinatario)
+    # print(destinatario)
     print(mensaje)
     test()
-    return '''<h1>The language value is: {} y {}</h1>'''.format(destinatario,mensaje)
+    return '''<h1>The language value is: {} y {}</h1>'''.format(mensaje,mensaje)
 
 def test():    
     url = 'https://live-server-763.wati.io/api/v1/sendSessionMessage/51937535378'
     auth_token='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJiODE0MjUzYy1mMDAyLTQ3MTYtYjkwOS0xMmJhY2E3MDc3ZDEiLCJ1bmlxdWVfbmFtZSI6ImFnZW50ZTVAdGVsZW5ldC5wZSIsIm5hbWVpZCI6ImFnZW50ZTVAdGVsZW5ldC5wZSIsImVtYWlsIjoiYWdlbnRlNUB0ZWxlbmV0LnBlIiwiYXV0aF90aW1lIjoiMDcvMTMvMjAyMSAxMzoxOTo0NCIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6IkFETUlOSVNUUkFUT1IiLCJleHAiOjI1MzQwMjMwMDgwMCwiaXNzIjoiQ2xhcmVfQUkiLCJhdWQiOiJDbGFyZV9BSSJ9.0YITXIKLxJHe5Prjt7O53ofcRvi0PNJb-U7TI06cRRE'
-    param = {'messageText':"texto de flask",}
+    param = {'messageText':mensaje,}
     headers = {'Content-type': 'application/json ; charset=UTF-8','Authorization': 'Bearer ' + auth_token}
     response = requests.post(url, params=param, headers=headers)
     # wait for the response. it should not be higher 
